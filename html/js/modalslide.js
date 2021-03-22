0c50b7ae353b7176dfcc58dceaa29f658c108ea8
jghee717 <jghee717@naver.com>
2021-03-22 15:43:56 +0900

    modal

---
+++ html/js/modalslide.js
@@ -0,0 +1,77 @@
+var $careNum = 0;
+var $theSizeiot = 6;
+var $theSize = 6;
+
+
+//iot
+$(".menu-left ul").hide();
+$(".menu-left ul").eq(0).show();
+$(".menu-left .prev").click(function() {
+  $careNum--;
+  if ($careNum < 0) $careNum = $theSizeiot - 1;
+  $(".menu-left ul").hide();
+  $(".menu-left ul").eq($careNum).show();
+});
+
+$(".control .next").click(function() {
+  $careNum++;
+  if ($careNum >= $theSizeiot) $careNum = 0;
+  $(this).parent().children("ul");
+  $(".menu-left ul").hide();
+  $(".menu-left ul").eq($careNum).show();
+});
+
+
+//qpet
+$(".menu-left2 ul").hide();
+$(".menu-left2 ul").eq(0).show();
+$(".menu-left2 .prev").click(function() {
+  $careNum--;
+  if ($careNum < 0) $careNum = $theSize - 1;
+  $(".menu-left2 ul").hide();
+  $(".menu-left2 ul").eq($careNum).show();
+});
+
+$(".control2 .next").click(function() {
+  $careNum++;
+  if ($careNum >= $theSize) $careNum = 0;
+  $(this).parent().children("ul");
+  $(".menu-left2 ul").hide();
+  $(".menu-left2 ul").eq($careNum).show();
+});
+
+//ChkHeat
+$(".menu-left3 ul").hide();
+$(".menu-left3 ul").eq(0).show();
+$(".menu-left3 .prev").click(function() {
+  $careNum--;
+  if ($careNum < 0) $careNum = $theSize - 1;
+  $(".menu-left3 ul").hide();
+  $(".menu-left3 ul").eq($careNum).show();
+});
+
+$(".control3 .next").click(function() {
+  $careNum++;
+  if ($careNum >= $theSize) $careNum = 0;
+  $(this).parent().children("ul");
+  $(".menu-left3 ul").hide();
+  $(".menu-left3 ul").eq($careNum).show();
+});
+
+//ProductOrder
+$(".menu-left4 ul").hide();
+$(".menu-left4 ul").eq(0).show();
+$(".menu-left4 .prev").click(function() {
+  $careNum--;
+  if ($careNum < 0) $careNum = $theSize - 1;
+  $(".menu-left4 ul").hide();
+  $(".menu-left4 ul").eq($careNum).show();
+});
+
+$(".control4 .next").click(function() {
+  $careNum++;
+  if ($careNum >= $theSize) $careNum = 0;
+  $(this).parent().children("ul");
+  $(".menu-left4 ul").hide();
+  $(".menu-left4 ul").eq($careNum).show();
+});
--- html/js/slidehover.js
+++ html/js/slidehover.js
@@ -1,25 +1,46 @@
 //슬라이드
-let currSlide = 1;
-showSlide(currSlide);
+// let currSlide = 1;
+// showSlide(currSlide);
+//
+// function button_click(num) {
+//   showSlide((currSlide += num));
+// }
+//
+// function showSlide(num) {
+//   const slides = document.querySelectorAll(".slide");
+//   if (num > slides.length) {
+//     currSlide = 1;
+//   }
+//   if (num < 1) {
+//     currSlide = slides.length;
+//   }
+//   for (let i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   slides[currSlide - 1].style.display = "block";
+//
+// }
 
-function button_click(num) {
-  showSlide((currSlide += num));
-}
 
-function showSlide(num) {
-  const slides = document.querySelectorAll(".slide");
-  if (num > slides.length) {
-    currSlide = 1;
-  }
-  if (num < 1) {
-    currSlide = slides.length;
-  }
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[currSlide - 1].style.display = "block";
-
-}
+// let currSlide2 = 1;
+// showSlide2(currSlide2);
+//
+// function button_click2(num2) {
+//   showSlide((currSlide2 += num2));
+// }
+// function showSlide2(num) {
+//   const slides2 = document.querySelectorAll(".sl2");
+//   if (num > slides2.length) {
+//     currSlide2 = 1;
+//   }
+//   if (num < 1) {
+//     currSlide2 = slides2.length;
+//   }
+//   for (let i = 0; i < slides2.length; i++) {
+//     slides2[i].style.display = "none";
+//   }
+//   slides2[currSlide2 - 1].style.display = "block";
+// }
 
 
 
